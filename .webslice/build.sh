7ad2ddc5c28457bb938f8d1204979bbb889c21a6
Tommy <61849757+tommy-ng0@users.noreply.github.com>
2026-05-15 10:52:18 +1200

    Update build.sh

--- .webslice/build.sh
+++ .webslice/build.sh
@@ -7,7 +7,8 @@ set -ex
 cp .env.example .env
 composer install --no-interaction -vvv --no-dev --prefer-dist --optimize-autoloader
 npm ci --cache .npm --prefer-offline
-npm run build:prod
+npm run dev
+
 php artisan key:generate
 
 echo "Build Script Finished Successfully"
