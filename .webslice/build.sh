f6f96200f6d7401dbc31183b42e7de559db8a70e
Tommy <61849757+tommy-ng0@users.noreply.github.com>
2026-05-15 10:45:50 +1200

    Create build.sh

---
+++ .webslice/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Starting Build Script"
+
+# Print command output for each command and exit immediately if any command fails (https://man7.org/linux/man-pages/man1/set.1p.html)
+set -ex
+
+cp .env.example .env
+composer install --no-interaction -vvv --no-dev --prefer-dist --optimize-autoloader
+npm ci --cache .npm --prefer-offline
+npm run build:prod
+php artisan key:generate
+
+echo "Build Script Finished Successfully"
